525b0cc8b987037be13cc6bd5e93e82ca586a569
joabe <joabbesouza@gmail.com>
2023-03-12 01:21:49 -0300

    email e senha salva no firebase e feito o tratamento das exceções

--- app/src/main/java/com/example/basicnotepad/Activities/TelaCadastro.kt
+++ app/src/main/java/com/example/basicnotepad/Activities/TelaCadastro.kt
@@ -1,12 +1,62 @@
 package com.example.basicnotepad.Activities
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
-import com.example.basicnotepad.R
+import android.view.Gravity
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.basicnotepad.databinding.ActivityTelaCadastroBinding
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.FirebaseNetworkException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthException
+import com.google.firebase.auth.FirebaseAuthUserCollisionException
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException
 
 class TelaCadastro : AppCompatActivity() {
+
+    private lateinit var binding: ActivityTelaCadastroBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_tela_cadastro)
+        binding = ActivityTelaCadastroBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        window.statusBarColor = Color.parseColor("#FF000000")
+        supportActionBar!!.hide()
+
+        binding.btCadastro.setOnClickListener {
+
+            val email = binding.editEmail.text.toString()
+            val senha = binding.editSenha.text.toString()
+
+            if (email.isEmpty() || senha.isEmpty()){
+                val snackbar = Snackbar.make(it,"Preencha todos os Dados!",Snackbar.LENGTH_SHORT)
+                snackbar.setAnchorView(it)
+                snackbar.show()
+            }else{
+                cadastro(it,email,senha)
+            }
+        }
+    }
+    private fun cadastro(view: View, email: String,senha: String){
+
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email,senha).addOnCompleteListener {
+            if (it.isSuccessful){
+                val snackbar = Snackbar.make(view,"Cadastro realizado com Sucesso!",Snackbar.LENGTH_SHORT)
+                snackbar.setAnchorView(view)
+                snackbar.show()
+            }
+        }.addOnFailureListener {
+            val erro = when(it){
+                is FirebaseAuthWeakPasswordException -> "Necessário uma senha com no mínimo 6 caracteres!"
+                is FirebaseAuthUserCollisionException -> "Este E-mail já foi usado!"
+                is FirebaseNetworkException -> "Sem conexão com a Internet!"
+                else -> "Erro ao cadastrar o usuário!"
+            }
+            val snackbar = Snackbar.make(view,erro,Snackbar.LENGTH_SHORT)
+            snackbar.setAnchorView(view)
+            snackbar.show()
+        }
     }
 }
--- app/src/main/java/com/example/basicnotepad/Activities/TelaEntrar.kt
+++ app/src/main/java/com/example/basicnotepad/Activities/TelaEntrar.kt
@@ -3,10 +3,19 @@ package com.example.basicnotepad.Activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.basicnotepad.R
+import com.example.basicnotepad.databinding.ActivityTelaEntrarBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.ktx.Firebase
 
 class TelaEntrar : AppCompatActivity() {
+
+    private lateinit var binding: ActivityTelaEntrarBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_tela_entrar)
+        binding = ActivityTelaEntrarBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    fun logarUsuario(){
     }
 }
