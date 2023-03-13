1244567a37ac363da15e89e4e1046ff99d07cf98
joabe <joabbesouza@gmail.com>
2023-03-13 14:39:20 -0300

    menu faltas, frequência, mural e contatos adicionado na tela cadastro

--- app/src/main/java/com/example/basicnotepad/Activities/TelaPrincipal.kt
+++ app/src/main/java/com/example/basicnotepad/Activities/TelaPrincipal.kt
@@ -1,11 +1,14 @@
 package com.example.basicnotepad.Activities
 
+import android.content.Intent
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import com.example.basicnotepad.R
 import com.example.basicnotepad.databinding.ActivityTelaPrincipalBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class TelaPrincipal : AppCompatActivity() {
 
@@ -18,10 +21,23 @@ class TelaPrincipal : AppCompatActivity() {
 
         window.statusBarColor = Color.parseColor("#FF000000")
     }
-
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_principal,menu)
         return true
     }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+           // R.id.perfil -> iniciarDialogperfil()
+            R.id.sair -> deslogarUsuario()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    fun deslogarUsuario(){
+        FirebaseAuth.getInstance().signOut()
+        val intent = Intent(this,TelaLogin::class.java)
+        startActivity(intent)
+        finish()
+    }
 }
